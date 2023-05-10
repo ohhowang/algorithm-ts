53c0645cfeed347b939854c660ba25e149887a97
ohhowang <ohhowang@gmail.com>
2023-05-10 11:50:30 +0800

    feat: another way implements array queue

--- src/queue/array-queue.ts
+++ src/queue/array-queue.ts
@@ -1,19 +1,34 @@
 export class ArrayQueue<T extends any> {
+  front = -1;
+  rear = -1;
   list: T[] = [];
 
-  constructor() {}
+  constructor() {
+    this.front = 0;
+    this.rear = 0;
+  }
+
   /**
    * 添加到尾部，true 异常
    */
   add(val: T) {
     this.list.push(val);
+    this.rear++;
   }
+
   /**
    * true false
    */
   remove() {
-    this.list.shift();
+    // this.list.shift();
+    if (this.front === this.rear) return;
+
+    for (let i = 0; i < this.rear; i++) {
+      this.list[i] = this.list[i + 1];
+    }
+    this.rear--;
   }
+
   /**
    * 获取第一个元素，null
    */
